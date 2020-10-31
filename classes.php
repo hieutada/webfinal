16661b665e809243ba4061c719c359638d33b052
Ta Trung Hieu <you@example.comhieutada2k@gmail.com>
2020-10-31 12:17:53 +0700

    Them phan danh muc ben trai

--- classes.php
+++ classes.php
@@ -18,7 +18,7 @@
             <!-- Logo link -->
             <div class="navbar-header">
                 <!-- Tên lớp học -->
-                <a class="navbar-brand" href="#">IELTS Dan</a> 
+                <a class="navbar-brand" href="#">IELTS Dan</a>
             </div>
 
             <!-- Nut menu khi thay doi ti le man hinh -->
@@ -27,7 +27,7 @@
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="nav nav-tabs mx-auto justify-content-center" id="myTab" role="tablist">
+                <ul class="nav nav-tabs justify-content-center" id="myTab" role="tablist">
                     <li class="nav-item">
                         <a class="nav-link active" id="stream-tab" data-toggle="tab" href="#stream" role="tab" aria-controls="stream" aria-selected="true">Stream</a>
                     </li>
@@ -55,80 +55,132 @@
         </div>
     </nav>
     <!--  -->
-    <div class="tab-content container" id="myTabContent">
-
-        <!-- Stream tab-->
-        <div class="tab-pane fade show active mt-3" id="stream" role="tabpanel" aria-labelledby="stream-tab">
-
-            <!-- Up status/document -->
-            <form class="border rounded mb-3" action="" method="post">
-                <!-- text input -->
-                <div class="form-group px-2 pt-3">
-                    <label for="exampleFormControlTextarea1">Share with your class</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                </div>
-                <div class="form-group row px-2">
-                    <!-- up file -->
-                    <div class="col-sm pb-2">
-                        <div class="custom-file">
-                            <input type="file" class="custom-file-input" id="customFile">
-                            <label class="custom-file-label" for="customFile"></label>
+    <div class="container-fluid">
+        <div class="row mt-3">
+            <div class="col-sm-3">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-home" aria-hidden="true"></i>
+                            Classroom
+                        </a>
+                    </li>
+                    <li class="list-group-item">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-tasks" aria-hidden="true"></i>
+                            To-do
+                        </a>
+                    </li>
+                    <li class="list-group-item ">
+                        <p>Registered:</p>
+                        <div class="overflow-auto" style="max-height: 100px;">
+                            <div class="d-flex bd-highlight mb-2">
+                                <img class="rounded-circle" src="https://via.placeholder.com/40x40?text=Avt" alt="Teacher Image">
+                                <a class="nav-link" href="#">IELTS Dan</a>
+                            </div>
+                            <div class="d-flex bd-highlight mb-2">
+                                <img class="rounded-circle" src="https://via.placeholder.com/40x40?text=Avt" alt="Teacher Image">
+                                <a class="nav-link" href="#">Data Structure</a>
+                            </div>
+                            <div class="d-flex bd-highlight mb-2">
+                                <img class="rounded-circle" src="https://via.placeholder.com/40x40?text=Avt" alt="Teacher Image">
+                                <a class="nav-link" href="#">C/C++</a>
+                            </div>
                         </div>
-                        <script>
-                            $(".custom-file-input").on("change", function() {
-                                var fileName = $(this).val().split("\\").pop();
-                                $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-                            });
-                        </script>
-                    </div>
-                    <!-- submit document -->
-                    <div class="col-sm pb-2">
-                        <button type="submit" class="btn btn-primary ">Post</button>
-                    </div>
-                </div>
-            </form>
+                    </li>
+                    <li class="list-group-item">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-archive" aria-hidden="true"></i>
+                            Storage
+                        </a>
+                    </li>
+                    <li class="list-group-item">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-cog" aria-hidden="true"></i>
+                            Setting
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-sm 9">
+                <div class="tab-content" id="myTabContent">
+                    <!-- Stream tab-->
+                    <div class="tab-pane fade show active" id="stream" role="tabpanel" aria-labelledby="stream-tab">
 
-            <!-- document link card -->
-            <div class="card mb-3">
-                <div class="card-body d-flex">
-                    <div class="align-self-center mr-3">
-                        <i class="fa fa-bookmark-o fa-2x" aria-hidden="true"></i>
-                    </div>
-                    <div style="flex: 1;">
-                        <a href="#" class="font-weight-bold text-dark stretched-link">Keynote 3</a>
-                        <!-- <div class="font-weight-normal">Date post: 20/10/2020</div> -->
-                        <div>
-                            <small>Date post: 20/10/2020</small>
+                        <!-- Up status/document -->
+                        <form class="border rounded mb-3" action="" method="post">
+                            <!-- text input -->
+                            <div class="form-group px-2 pt-3">
+                                <label for="exampleFormControlTextarea1">Share with your class</label>
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            </div>
+                            <div class="form-group row pl-2">
+                                <!-- up file -->
+                                <div class="col-sm pb-2">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" id="customFile">
+                                        <label class="custom-file-label" for="customFile"></label>
+                                    </div>
+                                    <script>
+                                        $(".custom-file-input").on("change", function() {
+                                            var fileName = $(this).val().split("\\").pop();
+                                            $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+                                        });
+                                    </script>
+                                </div>
+                                <!-- submit document -->
+                                <div class="col-sm pb-2">
+                                    <button type="submit" class="btn btn-primary ">Post</button>
+                                </div>
+                            </div>
+                        </form>
+
+                        <!-- document link card -->
+                        <div class="card mb-3">
+                            <div class="card-body d-flex">
+                                <div class="align-self-center mr-3">
+                                    <i class="fa fa-bookmark-o fa-2x" aria-hidden="true"></i>
+                                </div>
+                                <div style="flex: 1;">
+                                    <a href="#" class="font-weight-bold text-dark stretched-link">Keynote 3</a>
+                                    <!-- <div class="font-weight-normal">Date post: 20/10/2020</div> -->
+                                    <div>
+                                        <small>Date post: 20/10/2020</small>
+                                    </div>
+                                </div>
+                                <button class="btn"><i class="fa fa-ellipsis-v fa-lg" aria-hidden="true"></i></button>
+                            </div>
                         </div>
                     </div>
-                    <button class="btn"><i class="fa fa-ellipsis-v fa-lg" aria-hidden="true"></i></button>
+
+                    <!-- Classwork tab -->
+                    <div class="tab-pane fade" id="classwork" role="tabpanel" aria-labelledby="classwork-tab">
+
+                    </div>
+
+                    <!-- People tab -->
+                    <div class="tab-pane fade mt-3" id="people" role="tabpanel" aria-labelledby="people-tab">
+                        <!-- teacher -->
+                        <div class="h4 font-weight-normal text-primary">Teacher</div>
+                        <hr>
+                        <div class="d-flex bd-highlight mt-3">
+                            <img class="rounded-circle mr-3" src="https://via.placeholder.com/40x40?text=Avt" alt="Teacher Image">
+                            <div class="align-self-center">Trần Thi Đan</div>
+                        </div>
+                        <br><br>
+                        <!-- classmate -->
+                        <span class="h4 font-weight-normal text-primary mr-2">Classmates</span><span class="align-self-center">(2 Students)</span>
+                        <hr>
+                        <div class="d-flex bd-highlight mt-2">
+                            <img class="rounded-circle mr-3" src="https://via.placeholder.com/40x40?text=Avt" alt="Teacher Image">
+                            <div class="align-self-center">Tạ Trung Hiếu</div>
+                        </div>
+                        <div class="d-flex bd-highlight mt-3">
+                            <img class="rounded-circle mr-3" src="https://via.placeholder.com/40x40?text=Avt" alt="Teacher Image">
+                            <div class="align-self-center">Phạm Hà Giang</div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-
-        <!-- Classwork tab -->
-        <div class="tab-pane fade" id="classwork" role="tabpanel" aria-labelledby="classwork-tab">
-
-        </div>
-
-        <!-- People tab -->
-        <div class="tab-pane fade mt-3" id="people" role="tabpanel" aria-labelledby="people-tab">
-            <!-- teacher -->
-            <div class="h4 font-weight-normal text-primary">Teacher</div> <hr>
-            <div class="d-flex bd-highlight mt-3">
-                <img class="rounded-circle mr-3" src="https://via.placeholder.com/40x40?text=Avt" alt="Teacher Image">
-                <div class="align-self-center">Trần Thi Đan</div>
-            </div>
-            <br><br>
-            <!-- classmate -->
-            <span class="h4 font-weight-normal text-primary mr-2">Classmates</span><span class="align-self-center">(2 Students)</span><hr>
-            <div class="d-flex bd-highlight mt-2">
-                <img class="rounded-circle mr-3" src="https://via.placeholder.com/40x40?text=Avt" alt="Teacher Image">
-                <div class="align-self-center">Tạ Trung Hiếu</div>
-            </div>
-            <div class="d-flex bd-highlight mt-3">
-                <img class="rounded-circle mr-3" src="https://via.placeholder.com/40x40?text=Avt" alt="Teacher Image">
-                <div class="align-self-center">Phạm Hà Giang</div>
             </div>
         </div>
     </div>
