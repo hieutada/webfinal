1cab13dc486ae8aaa9aadca1367119a67793aaae
Ta Trung Hieu <you@example.comhieutada2k@gmail.com>
2020-11-15 02:28:35 +0700

    Cập nhật UI Assignment v.1

--- todo.php
+++ todo.php
@@ -1,4 +1,15 @@
 <!DOCTYPE html>
+
+<?php
+session_start();
+if (!isset($_SESSION['take_id_user'])) {
+    header("Location: login.php");
+} else {
+    require 'connect.php';
+    $id_user = $_SESSION['take_id_user'];
+}
+?>
+
 <html lang="en">
 
 <head>
@@ -11,45 +22,16 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="css/simple-sidebar.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
 </head>
 
 <body>
     <div class="d-flex" id="wrapper">
         <!-- 1. Sidebar -->
-        <div class="bg-light border-right" id="sidebar-wrapper" style="margin-top: 60px;">
-            <div class="list-group list-group-flush">
-                <a href="home.php" class="list-group-item list-group-item-action bg-light">
-                    <i class="fa fa-home" aria-hidden="true"></i> Classroom
-                </a>
-                <a href="todo.php" class="list-group-item list-group-item-action bg-light">
-                    <i class="fa fa-tasks" aria-hidden="true"></i> To-do
-                </a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">
-                    <i class="fa fa-archive" aria-hidden="true"></i> Storage
-                </a>
-                <div class="list-group-item list-group-item-action bg-light">
-                    <p>Registered:</p>
-                    <div class="overflow-auto">
-                        <div class="d-flex bd-highlight mb-2">
-                            <img class="rounded-circle" src="https://via.placeholder.com/40x40?text=Avt" alt="Teacher Image" style="width: 40px; height:40px">
-                            <a class="nav-link" href="#">IELTS Dan</a>
-                        </div>
-                        <div class="d-flex bd-highlight mb-2">
-                            <img class="rounded-circle" src="https://via.placeholder.com/40x40?text=Avt" alt="Teacher Image" style="width: 40px; height:40px">
-                            <a class="nav-link" href="#">Data Structure</a>
-                        </div>
-                        <div class="d-flex bd-highlight mb-2">
-                            <img class="rounded-circle" src="https://via.placeholder.com/40x40?text=Avt" alt="Teacher Image" style="width: 40px; height:40px">
-                            <a class="nav-link" href="#">C/C++</a>
-                        </div>
-                    </div>
-                </div>
-                <a href="#" class="list-group-item list-group-item-action bg-light">
-                    <i class="fa fa-cog" aria-hidden="true"></i>
-                    Setting
-                </a>
-            </div>
+        <div class="bg-light border-right" id="sidebar-wrapper">
+            <?php
+            require "load_sidebar.php"
+            ?>
         </div>
 
         <!-- 2. Content -->
@@ -79,7 +61,7 @@
                     <!-- 2.1.3 Account item -->
                     <span class="nav-item dropdown">
                         <a class="nav-link dropdown p-0" href="#" id="dropdownMenuButton" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <img class="rounded-circle" src="https://via.placeholder.com/40x40?text=Avt" style="width:35px; height:35px">
+                            <img class="rounded-circle" src="https://via.placeholder.com/40x40?text=Avt">
                         </a>
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
                             <a class="dropdown-item" href="profile.php"><i class="fa fa-user"></i> Profile</a>
@@ -93,8 +75,8 @@
             <!-- 2.2 Tab content -->
             <div class="container-md tab-content" id="myTabContent" style="margin-top: 80px;">
                 <!-- 2.2.0 Lựa chọn lớp học -->
-                <form action="" method="post">
-                    <div class="form-group" style="width: 200px;">
+                <form class="row" action="" method="post">
+                    <div class="form-group col-3">
                         <select class="form-control" id="exampleFormControlSelect1">
                             <option>All classes</option>
                             <option>IELTS Dan</option>
@@ -108,39 +90,86 @@
                     <!-- Load card bài tập tại đây-->
                     <div class="card mb-3">
                         <div class="card-body d-flex">
-                            <div class="align-self-center mr-3">
+                            <span class="align-self-center mr-3">
                                 <i class="fa fa-sticky-note-o fa-2x" aria-hidden="true"></i>
-                            </div>
-                            <div style="flex: 1;">
-                                <a href="#" class="font-weight-bold text-dark stretched-link">Writing task 1</a>
-                                <!-- <div class="font-weight-normal">Date post: 20/10/2020</div> -->
-                                <div><small>IELTS Dan</small></div>
-                                <div><small>Date post: 20/10/2020</small></div>
-                            </div>
-                            <button class="btn"><i class="fa fa-ellipsis-v fa-lg" aria-hidden="true"></i></button>
+                            </span>
+                            <span>
+                                <a href="assignment.php" class="font-weight-bold text-dark stretched-link">Tên bài tập</a>
+                                <div><small>Lớp học</small></div>
+                                <div><small>Ngày post:</small></div>
+                            </span>
+                            <button class="btn ml-auto"><i class="fa fa-ellipsis-v fa-lg" aria-hidden="true"></i></button>
                         </div>
                     </div>
                 </div>
 
                 <!-- 2.2.2 Missing tab -->
                 <div class="tab-pane fade mt-3" id="missing" role="tabpanel" aria-labelledby="missing-tab">
-                    Bo Lo
+                    <!-- Load card bài tập missing tại đây-->
+                    <div class="card mb-3">
+                        <div class="card-body d-flex">
+                            <span class="align-self-center mr-3">
+                                <i class="fa fa-sticky-note-o fa-2x" aria-hidden="true"></i>
+                            </span>
+                            <span>
+                                <a href="assignment.php" class="font-weight-bold text-dark stretched-link">Tên bài tập</a>
+                                <div><small>Lớp học</small></div>
+                            </span>
+                            <div class="align-self-center ml-auto text-danger"><b>Wednesday, Oct 30, 2019</b></div>
+                        </div>
+                    </div>
                 </div>
 
                 <!-- 2.2.3. Done tab -->
                 <div class="tab-pane fade mt-3" id="done" role="tabpanel" aria-labelledby="done-tab">
-                    Hoan Thanh
+                    <!-- Đã cho điểm -->
+                    <div class="card mb-3">
+                        <div class="card-body d-flex">
+                            <span class="align-self-center mr-3">
+                                <i class="fa fa-sticky-note-o fa-2x" aria-hidden="true"></i>
+                            </span>
+                            <span>
+                                <a href="assignment.php" class="font-weight-bold text-dark stretched-link">Tên bài tập</a>
+                                <div><small>Lớp học</small></div>
+                            </span>
+                            <div class="align-self-center ml-auto text-secondary"><b>9.5/10</b></div>
+                        </div>
+                    </div>
+                    <!-- Đã nộp -->
+                    <div class="card mb-3">
+                        <div class="card-body d-flex">
+                            <span class="align-self-center mr-3">
+                                <i class="fa fa-sticky-note-o fa-2x" aria-hidden="true"></i>
+                            </span>
+                            <span>
+                                <a href="assignment.php" class="font-weight-bold text-dark stretched-link">Tên bài tập</a>
+                                <div><small>Lớp học</small></div>
+                            </span>
+                            <div class="align-self-center ml-auto text-secondary"><b>Turned-in</b></div>
+                        </div>
+                    </div>
+                    <!-- Nộp trễ -->
+                    <div class="card mb-3">
+                        <div class="card-body d-flex">
+                            <span class="align-self-center mr-3">
+                                <i class="fa fa-sticky-note-o fa-2x" aria-hidden="true"></i>
+                            </span>
+                            <span>
+                                <a href="assignment.php" class="font-weight-bold text-dark stretched-link">Tên bài tập</a>
+                                <div><small>Lớp học</small></div>
+                            </span>
+                            <div class="align-self-center ml-auto text-secondary">
+                                <b>Turned-in</b> <br><small>(Done late)</small>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <script>
-        $("#menu-toggle").click(function(e) {
-            e.preventDefault();
-            $("#wrapper").toggleClass("toggled");
-        });
-    </script>
+    <!-- JS -->
+    <script src="main.js"></script>
 </body>
 
 </html>
